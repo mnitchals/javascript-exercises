18b39938ac4988bd4123c65036504d08b60e4212
Matthew Nitchals <matthew.nitchals@gmail.com>
2022-01-01 18:51:29 -0500

    made it work

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
+<<<<<<< Updated upstream
 const repeatString = function() {
 
+=======
+const repeatString = function(phrase, times) {
+    let returnString = '';
+    if (times < 0) {
+        return "ERROR"
+    }
+    else {
+        for (let x = 0; x < times; x++) {
+            returnString += phrase;
+        }
+        return returnString;
+    }
+>>>>>>> Stashed changes
 };
 
 // Do not edit below this line
