41a118b52f9306bce92555db7326f6c5bdc8b7d8
Matthew Nitchals <matthew.nitchals@gmail.com>
2022-01-02 09:58:16 -0500

    completd ex

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR"
+    } 
+    let sum = 0;
+    if (num1 > num2) {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    for (let x=num1; x<=num2;x++) {
+        sum += x;
+    }
+    return sum;
 };
 
 // Do not edit below this line
