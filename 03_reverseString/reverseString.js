ada628eb5e68cf003d4fbe634fd9abf336871345
Matthew Nitchals <matthew.nitchals@gmail.com>
2022-01-01 19:11:39 -0500

    reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
-
+    const strArray = str.split('');
+    let retStr = '';
+    for (let x=strArray.length -1; x > -1; x--) {
+        retStr += strArray[x];
+    }
+    return retStr;
 };
 
 // Do not edit below this line
