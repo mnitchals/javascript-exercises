c825db8a2efdf6fb2b0db106ff4032ae2a73770e
Matthew Nitchals <matthew.nitchals@gmail.com>
2022-01-02 08:37:41 -0500

    added indexof()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(origArray, ...args) {
+ let newArray = [];
+ for (let x=0; x < origArray.length; x++)
+ {
+     if (args.indexOf(origArray[x]) < 0) {
+        newArray.push(origArray[x])
+     }
+ }
+ return newArray;
 };
 
 // Do not edit below this line
